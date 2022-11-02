44a5fc78777110511059bcf27e9da625e31a40b4
Jeongpyo Hong <112886992+Jeongpyo-Hong@users.noreply.github.com>
2022-11-02 13:35:48 +0900

    머지 전

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,28 +1,31 @@
 import { useState, useEffect, useCallback } from "react";
 import { api } from "../shared/apis";
 
-function useFetch(page) {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [stores, setStores] = useState([]);
+function useFetch(page, categoryId) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [stores, setStores] = useState([]);
 
-    const sendQuery = useCallback(async () => {
-        try {
-            await setLoading(true);
-            await setError(false);
-            const res = await api.get(`/?page=${page}`);
-            await setStores((prev) => prev.concat(res.data.storeList));
-            setLoading(false);
-        } catch (err) {
-            setError(err);
-        }
-    }, [page]);
+  const sendQuery = useCallback(async () => {
+    try {
+      await setLoading(true);
+      await setError(false);
+      const res = await api.get(
+        `${categoryId === 0 ? `/?page=${page}` : `/${categoryId}?page=${page}`}`
+      );
+      console.log("Res", res);
+      await setStores((prev) => prev.concat(res.data.storeList));
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+    }
+  }, [page]);
 
-    useEffect(() => {
-        sendQuery(page);
-    }, [sendQuery, page]);
+  useEffect(() => {
+    sendQuery();
+  }, [sendQuery, page]);
 
-    return { loading, error, stores };
+  return { loading, error, stores };
 }
 
 export default useFetch;
