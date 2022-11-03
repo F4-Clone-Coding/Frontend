968d97dbe7594e0ebd56e3775b3837f904c26579
Jeongpyo Hong <112886992+Jeongpyo-Hong@users.noreply.github.com>
2022-11-03 10:21:09 +0900

    모든 기능은 로그인 후 가능하도록 변경

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { api } from "../shared/apis";
+import instance from '../shared/apis';
 
 function useFetch(page, categoryId) {
     const [loading, setLoading] = useState(true);
@@ -10,7 +10,7 @@ function useFetch(page, categoryId) {
         try {
             await setLoading(true);
             await setError(false);
-            const res = await api.get(`${categoryId === 0 ? `/?page=${page}` : `/${categoryId}?page=${page}`}`);
+            const res = await instance.get(`${categoryId === 0 ? `/?page=${page}` : `/${categoryId}?page=${page}`}`);
             console.log("Res", res)
             await setStores((prev) => prev.concat(res.data.storeList));
             setLoading(false);
